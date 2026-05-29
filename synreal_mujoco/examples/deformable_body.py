af3aadb26110356638186fb10f649683a1a18149
zeagle <zeagle01@163.com>
2026-05-29 17:41:25 +0800

    feat: more robust path

--- synreal_mujoco/examples/deformable_body.py
+++ synreal_mujoco/examples/deformable_body.py
@@ -1,17 +1,31 @@
 import mujoco.viewer
 
+import numpy as np
+
+# include parent folder
+import os
+import sys
+
+# Get parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, parent_dir)
+# include parent folder end
+
 import synreal_mujoco.s3d_mj as s3d_mj
 import synreal_mujoco.s3d_scene as s3d_scene
 import synreal_mujoco.s3d_scene_stepper as s3d_scene_stepper
-import numpy as np
 
-s3d_mj.log_in_simulation(login_file='../../simulation_login.json') # this line is optional, but a login prompt will pop up latter
+from pathlib import Path
+
+curr_folder=Path(__file__).parent
+login_file = curr_folder.parent.parent / 'simulation_login.json'
+s3d_mj.log_in_simulation(login_file=login_file) # this line is optional, but a login prompt will pop up latter
 
 s3d_scene_builder = s3d_scene.s3d_scene_builder()
-s3d_scene_builder.add_mjcf_rigidbodies('xml_projects/piper_secription/piper_description.xml')
+s3d_scene_builder.add_mjcf_rigidbodies(curr_folder/'xml_projects/piper_secription/piper_description.xml')
 
 ######### tets
-dfm_attrib = s3d_scene_builder.add_deformable_body_by_file('xml_projects/piper_secription/tets1.vtk')
+dfm_attrib = s3d_scene_builder.add_deformable_body_by_file(curr_folder/'xml_projects/piper_secription/tets1.vtk')
 dfm_attrib.attrib.youngsModulus = 1e5
 #dfm_attrib.get_rest_pos = lambda  x: x # alter rest pos
 dfm_attrib.get_pos = lambda  x: x + np.array([0,0,0.3]) # alter current pos
